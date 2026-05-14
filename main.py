f0b5bf845eee17a607d68137fe1e90498db53937
Hassan Ali <hassanalisid1996@gmail.com>
2026-05-13 18:16:14 -0700

    B: get game working

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
 import pygame
-from constants import *
-from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
+from player import Player
+from asteroid import Asteroid
+from shot import Shot
+from asteroidfield import AsteroidField
+from logger import log_state, log_event
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,6 +14,24 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Create object ot help track time
+    pygame_clock = pygame.time.Clock()
+    dt = 0
+
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    # Containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
+
+    player_object = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield_object = AsteroidField()
 
     while(True):
         log_state()
@@ -16,7 +39,31 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        # player_object.update(dt)
+        updatable.update(dt)
+        for thing in asteroids:
+            if thing.collides_with(player_object):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        
+        for thing in asteroids:
+            for shot_thing in shots:
+                if thing.collides_with(shot_thing):
+                    log_event("asteroid_shot")
+                    thing.split()
+                    shot_thing.kill()
+
+
+        # player_object.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        # pygame_clock.tick(60) will pause the gameloop until 1/60th of a second has passed
+        # will also return amount of time passed since last time it was called
+        dt = pygame_clock.tick(60) / 1000
+        # should print around 0.016 seconds i think
+        # print(dt)
 
     pygame.quit()
 
